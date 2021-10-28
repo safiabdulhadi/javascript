4440ee3d14b811d33fe6398c5acbcf2aed839c6c
abdulhadi <safiabdulhadi1994@gmail.com>
2021-10-28 11:59:30 +0200

    I created new variable i work on it  trying to understand the variables how does it work the Syntaxe

---
+++ code-structure.js
@@ -0,0 +1,14 @@
+
+/* Code structure
+The first thing we’ll study is the building blocks of code.
+
+Statements
+Statements are syntax constructs and commands that perform actions.
+
+We’ve already seen a statement, alert('Hello, world!'), which shows the message “Hello, world!”.
+
+We can have as many statements in our code as we want. Statements can be separated with a semicolon.
+
+For example, here we split “Hello World” into two alerts:*/
+alert('Helllo');
+alert('World');
--- js-variables.js
+++ js-variables.js
@@ -1,3 +1,33 @@
+/*
+Variables
+Most of the time, a JavaScript application needs to work with information. Here are two examples:
+
+1. An online shop – the information might include goods being sold and a shopping cart.
+2. A chat application – the information might include users, messages, and much more.
+Variables are used to store this information.
+
+A variable
+A variable is a “named storage” for data. We can use variables to store goodies, visitors, and other data.
+
+To create a variable in JavaScript, use the let keyword.
+
+The statement below creates (in other words: declares) a variable with the name “message”:*/
+let message1;
+message1 = 'Hello, World' // store the string 'hello' in the variable named message 
+alert(message1);
+let message2;
+message2 = 'Hello';
+alert(message2);
+
+
+
+
+
+
+
+
+
+
 const myName = 'abdulhadi';
 console.log(myName);
 let mySurname = 'safi';
