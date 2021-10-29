3b5bf6b34c3bc5fb8e35f8da39fd8dce994785de
abdulhadi <safiabdulhadi1994@gmail.com>
2021-10-29 13:42:03 +0200

    add data-types

---
+++ Data-types.js
@@ -0,0 +1,9 @@
+/*Data types
+A value in JavaScript is always of a certain type. For example, a string or a number.
+
+There are eight basic data types in JavaScript. Here, we’ll cover them in general and in the next chapters we’ll talk about each of them in detail.
+
+We can put any type in a variable. For example, a variable can at one moment be a string and then store a number:*/
+// no error
+let message = "Hello";
+alert(message);
