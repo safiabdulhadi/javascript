0f2ce9ed1a9ce6627ca697439f68175005780b3c
abdulhadi <safiabdulhadi1994@gmail.com>
2021-10-29 14:01:37 +0200

    Strings example and Backticks:

--- Data-types.js
+++ Data-types.js
@@ -10,6 +10,28 @@ alert(message);
 */
 // Javascript Data Types
 // In this tutrial, you will learn about the various data types available  JavaScript with the help of examples.
+/* 
 const x = 5;
 const y = "Hello";
-console.log(x + ' '+ y );
+console.log(x + ' '+ y ); // for spaces (x + ' '+y)
+
+Data Types in javascript
+String     represents textual data  'hello' , "hello world!"
+number     an integer or a floating-point 3, 23', 2e-2 etc...
+BigInt     an interger with arbirary percision   9007199955151515n , 1n
+Boolean    Any of two values: true or false       true and false
+undefined  a data type whose variable is not initialized    let a;
+null       denotes a null value                   let a = null;
+Symbol     data type whose instances are unique and immutable      let value = Symbol('hello');
+object     key-value pairs of collection of data       let student = { };
+*/
+// strings example 
+/*
+const name = 'ram';
+const name1 = 'har';
+const result = `the names are ${name} and ${name1}`;*/
+//strings example
+const name = 'ram';
+const name1 = "hari";
+const result = `The names are ${name} and ${name1}`;
+console.log(result);
