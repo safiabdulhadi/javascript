3101f568cf890796c71a9f4d81f8f65d3ed4055a
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-10-31 20:01:36 +0100

    add new code prompt from w3school

--- prompt.js
+++ prompt.js
@@ -4,4 +4,21 @@ alert(name);
 'use strict'
 let name = prompt("What is your name ?", "");
 alert(name);
+
+function myFunction (){ 
+var person = prompt ("<Please enter your name", "Safi abdulhadi" );
+
+if (person != null) {
+       document.getElementById("demo").innerHTML = 
+       "Hello " + person + "! How are you today?";
+}
+}
 */
+function myFunction (){
+       var person = prompt ("Please enter your name", "abdul hadi");
+       if (person != null){ 
+              document.getElementById("demo").interHTML = 
+              "Hello " + person + "! How are you today?";
+              
+       }
+}
