b52bcf5ff63db543868e27120c892df5332861b8
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-01 17:41:43 +0100

    i modified and i add variable to check it out

--- loop.js
+++ loop.js
@@ -1,6 +1,7 @@
-let friendsAtYourParty = 0;
+/*let friendsAtYourParty = 0;
 while (friendsAtYourParty < 10) {
        friendsAtYourParty = friendsAtYourParty + 1;
 }
 console.log(friendsAtYourParty);
 // The answer wil be 10 
+*/
---
+++ statments.js
@@ -0,0 +1,19 @@
+/*var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18 ){
+       greeting = 'Good evening ';
+} else if (hourNow > 12) {
+       greeing = ' Good afternoon';
+} else if (hourNow > 0 ) {
+       greeting = 'Good morning';
+} else {
+       greeting = 'Welcome';
+}
+document.write(greeting);
+*/
+var x = 5;
+var y = 5;
+var z = x + y ;
+console.log(z);
