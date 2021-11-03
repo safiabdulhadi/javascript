489cfcc88d7a75c8ce98ec41adcce1778f0416d0
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-03 21:09:06 +0100

    modified i add for loop

--- loop.js
+++ loop.js
@@ -34,9 +34,14 @@ do{
 for (let i = 0; i<3; i++){
        alert(i); //0, 1, 2
 }
-*/
+
 let i = 0;
 for (i = 0; i <3; i++){
        alert(i); //0, 1, 2
 }
 alert(i); //visible, because declared outsid of the loop
+*/
+let i = 0; //we have i already declared and assigned 
+for (; i < 3; i++) { // no need for "begin"
+       alert(i); //0, 1, 2
+}
