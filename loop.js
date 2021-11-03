54121566a3542c77c3af556173e5bf6c5a7a8e30
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-03 14:38:27 +0100

    I trying to understand while loop

--- loop.js
+++ loop.js
@@ -5,3 +5,8 @@ while (friendsAtYourParty < 10) {
 console.log(friendsAtYourParty);
 // The answer wil be 10 
 */
+let i = 0;
+while (i<3){ //shows 0, then 1, then 2 
+       alert(i);
+       i++;
+}
