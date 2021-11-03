81a080114aeb0dca1ad7c09c6964c81523492b0c
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-03 14:44:48 +0100

    loop modified

--- loop.js
+++ loop.js
@@ -4,9 +4,24 @@ while (friendsAtYourParty < 10) {
 }
 console.log(friendsAtYourParty);
 // The answer wil be 10 
-*/
+
 let i = 0;
 while (i<3){ //shows 0, then 1, then 2 
        alert(i);
        i++;
 }
+
+
+let i = 0;
+while (i<10) {
+       alert(i);
+       i++;
+}
+
+let i = 3;
+while(i){
+       //when i becomes 0, the condition become falsy, and the loop stops 
+       alert(i);
+       i--;
+}
+*/
