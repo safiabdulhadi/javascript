3b24ab41d27908cf24b17a37fa0274907e474bac
safiabdulhadi <safiabdulhadi1994@gmail.com>
2021-11-09 20:38:07 +0100

    fucntion

--- functions.js
+++ functions.js
@@ -1,4 +1,11 @@
-function showMessage() {
+/*function showMessage() {
     alert('Hello everyone');
 }
 showMessage();
+*/
+function showMessage () {
+    let message = "Hello , I'm JavaScript! "; // local variable
+    alert(message);
+}
+
+showMessage(); // hello , I m JavaScript !
