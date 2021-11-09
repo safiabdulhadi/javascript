e259deb291e6128bdd21e07996dd9bdf0625a556
safiabdulhadi <safiabdulhadi1994@gmail.com>
2021-11-09 20:41:55 +0100

    commit function still

--- functions.js
+++ functions.js
@@ -2,10 +2,17 @@
     alert('Hello everyone');
 }
 showMessage();
-*/
+
 function showMessage () {
     let message = "Hello , I'm JavaScript! "; // local variable
     alert(message);
 }
 
 showMessage(); // hello , I m JavaScript !
+*/
+let userName = 'John';
+function showMessage () {
+    let message ='Hello, ' + userName;
+alert(message);
+}
+showMessage(); // Hello , John
