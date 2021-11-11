02fdb63e34e21f042af71531c7c361ab095ba697
safiabdullhadi <safiabdulhadi1994@gmail.com>
2021-11-11 19:29:42 +0100

    more add function expression

--- function-expressions.js
+++ function-expressions.js
@@ -1,4 +1,19 @@
+/*
 function sayHi(){
        alert("Hello");
 }
 alert(sayHi);
+*/
+function ask(question, yes , no){
+       if (confirm(question)) yes() 
+       else no();
+}
+function showOk() {
+       alert("You agreed.");
+
+}
+function showCancel () {
+       alert ("You canceled the execution.");
+}
+// usage: functions showOk, showCancel are passed as arguments to ask 
+ask ("Do you agree?", showOk, showCancel);
