420f041098962c04f4ae37635786a91b2b2439be
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2021-12-16 14:17:17 +0100

    commit function

--- function-expressions.js
+++ function-expressions.js
@@ -59,3 +59,18 @@ let func = sayHi;
 func();
 sayHi();
 */
+function ask (question, yes , no) {
+       if (confirm (question)) yes ()
+       else no ();
+}
+function showOk() {
+       alert ("You agreed.")
+
+}
+
+function showCancel() {
+       alert("<You canceled the excution.");
+     
+}
+// utilistion : les fonctions showOk, showCancel sont transmises en tant qu'arguments à ask 
+ask ("Do you agree ?", showOk, showCancel);
