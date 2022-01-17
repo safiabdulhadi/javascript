c97faa15a3fdfdb091170bf5c477f377067619f4
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-01-17 11:59:27 +0100

    its a bit complicate to understand

--- promises.js
+++ promises.js
@@ -1,5 +1,11 @@
 const btnJob = document.getElementById('btn-job');
 
+
+const candidat = {
+    isProgrammer: true,
+    isCool: true,
+};
+
 btnJob.addEventListener('click', applyToJob);
 
 function applyToJob(){
@@ -9,5 +15,15 @@ function applyToJob(){
     }, 2000);
     console.log(`Recruteur - Pendant ce temps je reçois d'autres candidats`);
 
+}
+function startDesicionProcess() {
+    return new Promise((resolve, reject)=> {
+        if (candidat.isProgrammer && candidat.isCool){
+            resolve('Recruteur - Bienvenue dans notre entrepris!')
+         } else {
+                reject('Reucruteur - Malgré tout 1\' intérêt de votre candidtur, nous sommes au regret de bla bla  ')
+            }
+
+    });
 }
 console.log('Aprés la funciton applyToJob');
