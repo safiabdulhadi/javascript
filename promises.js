53da278da60629cbb9f1ffa650b8f7b384d07602
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-01-18 13:26:38 +0100

    commit behavior js code

--- promises.js
+++ promises.js
@@ -1,29 +1,30 @@
-const btnJob = document.getElementById('btn-job');
+// const btnJob = document.getElementById('btn-job');
 
 
-const candidat = {
-    isProgrammer: true,
-    isCool: true,
-};
+// const candidat = {
+//     isProgrammer: true,
+//     isCool: true,
+// };
 
-btnJob.addEventListener('click', applyToJob);
+// btnJob.addEventListener('click', applyToJob);
 
-function applyToJob(){
-    console.log('La décision est en cours...');
-    setTimeout(() => {
-        console.log('Vous avez le job');
-    }, 2000);
-    console.log(`Recruteur - Pendant ce temps je reçois d'autres candidats`);
+// function applyToJob(){
+//     console.log('La décision est en cours...');
+//     setTimeout(() => {
+//         console.log('Vous avez le job');
+//     }, 2000);
+//     console.log(`Recruteur - Pendant ce temps je reçois d'autres candidats`);
 
-}
-function startDesicionProcess() {
-    return new Promise((resolve, reject)=> {
-        if (candidat.isProgrammer && candidat.isCool){
-            resolve('Recruteur - Bienvenue dans notre entrepris!')
-         } else {
-                reject('Reucruteur - Malgré tout 1\' intérêt de votre candidtur, nous sommes au regret de bla bla  ')
-            }
+// }
+// function startDesicionProcess() {
+//     return new Promise((resolve, reject)=> {
+//         if (candidat.isProgrammer && candidat.isCool){
+//             resolve('Recruteur - Bienvenue dans notre entrepris!')
+//          } else {
+//                 reject('Reucruteur - Malgré tout 1\' intérêt de votre candidtur, nous sommes au regret de bla bla  ')
+//             }
 
-    });
-}
-console.log('Aprés la funciton applyToJob');
+//     });
+// }
+// console.log('Aprés la funciton applyToJob');
+  
