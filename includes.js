e97f087b98b0271459298591c82938dd4f3b6505
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-01-30 20:35:30 +0100

    add includes

---
+++ includes.js
@@ -0,0 +1,33 @@
+// let arr1 = ['Ahmad', 'Karam', 'Javad' , 'Ahmad', 'Abdulhadi', 'karm'];
+// // let result = arr1.includes('Javad');
+
+// // document.write(result);
+
+// let result = '';
+// for(i = 0; i < arr1.length; i++){
+//   if(!result.includes([i]))
+//   {
+//       result += arr1[i];
+//   }
+  
+// }
+// document.write(result);
+
+// let array1 = ['ahmad' ,'bari','sadat','ahmad'];
+
+// let result = '';
+// for(i = 0; i < array1.length; i++)
+// {
+//     if(!result.includes(array1[i]))
+//     {
+//         result += array1[i];
+//     }
+// }
+// document.write(result);
+const array = [11, 12, 15, 30];
+console.log(array.includes(11));
+
+
+const array2 = ['blue', 'yellow', 'orange'];
+
+console.log(array2.includes('yellow'));
