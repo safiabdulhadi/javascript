633516ec53b10759a1879502bdac5e7b813ffd6f
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-01-30 18:06:53 +0100

    i have to paractic more includes

--- forMySelf.js
+++ forMySelf.js
@@ -121,7 +121,35 @@
 
 
 
-let arr1 = ['Ahmad', 'Karam', 'Javad' , 'Abdulhadi'];
-let arr2 = [90, 34, 45, 100];
-let result = arr1.indexOf('Abdulhadi');
+
+// let arr2 = [90, 34, 45, 100];
+// let result = arr1.indexOf('Abdulhadi');
+// document.write(result);
+
+
+// let arr1 = ['Ahmad', 'Karam', 'Javad' , 'Ahmad', 'Abdulhadi', 'karm'];
+// // let result = arr1.includes('Javad');
+
+// // document.write(result);
+
+// let result = '';
+// for(i = 0; i < arr1.length; i++){
+//   if(!result.includes([i]))
+//   {
+//       result += arr1[i];
+//   }
+  
+// }
+// document.write(result);
+
+let array1 = ['ahmad' ,'bari','sadat','ahmad'];
+
+let result = '';
+for(i = 0; i < array1.length; i++)
+{
+    if(!result.includes(array1[i]))
+    {
+        result += array1[i];
+    }
+}
 document.write(result);
