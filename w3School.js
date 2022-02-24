c60051b76148fd1ba664b98a43370ec19640a2dc
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-02-24 16:17:59 +0100

    started understanding the functions in js

--- w3School.js
+++ w3School.js
@@ -12,6 +12,34 @@
 // function clickOff(){
 //     document.getElementById('light').src = lightName;
 // }
-function test() {
-    document.getElementById("div1").innerHTML = 'Abdulhadi';
-}
+
+// let name = 'Abdulhadi safi';
+// let name = 'Abdulhadi\' safi';
+// let name = 'Abdulhadi\\ safi';
+// let name = 'Abdulhadi \b safi';
+// let name = 'Abdulhadi \f safi';
+// let name = 'Abdulhadi \safi';
+
+
+
+
+
+// function multiply(x,y) {
+//     return (x + y)
+// }
+
+
+// function test() {
+
+//     // document.getElementById("div1").innerHTML = name;
+//     // document.getElementById("div1").innerHTML = 'Abdulhadi' ;
+//     //  document.getElementById("div1").innerHTML = 'Abdul \hadi' ;
+// // }
+// const multiply = (x,y) => {
+//     let z = 10;
+//     return (x + z);
+//     // return (x + y);
+// }
+// sum = multiply(1,2);
+// document.getElementById("div1").innerHTML =" Sum = " + sum;
+
