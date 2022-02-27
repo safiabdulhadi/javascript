6e5907eebe4ff97f55ef053887297547124149b5
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-02-27 14:21:12 +0100

    obj

--- Aystemchpashto.js
+++ Aystemchpashto.js
@@ -4,9 +4,37 @@
 // var b = "Nawid";
 // const c = 1000;
 // document.write(c);
-const ages = [18, 39, 35,40,80];
-ages[1] = 'ahmmad';
-ages[0] = 'My name is ';
+// const ages = [18, 39, 35,40,80];
+// ages[1] = 'ahmmad';
+// ages[0] = 'My name is ';
 
 
-document.write(ages);
+// document.write(ages);
+// with const we can't inisalisze or declarare 
+// var ages = [18, 39, 35,40,80];
+// ages[1] = 'ahmmad';
+// ages[0] = 'My name is ';
+
+// ages = ['khan', 'ahmmad', 'sadat','nawid','names'];
+
+
+// document.write(ages);
+
+
+// var obj = {
+//     fristName: "ahmad",
+//     lastName: "khan",
+//     ages: 27
+// }
+// // for adding new value in obj
+// // obj.fristName = 'safi';
+
+// var obj = {
+//     fristName: "gullkhan",
+//     lastName: "malik",
+//     ages: 28
+// }
+
+
+
+// console.log(obj);
