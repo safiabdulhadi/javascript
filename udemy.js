29f0199cb3b7e9d2fcc92988ed8c7a3a555be27c
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-03-02 15:43:19 +0100

    tring in understand the document.querySelector and all

--- udemy.js
+++ udemy.js
@@ -1,5 +1,17 @@
 // const para = document.querySelector('p');
-const para = document.querySelector('body > h1');
+// const para = document.querySelector('body > h1');
+
+// console.log(para);
 
 
-console.log(para);
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para =>{
+//     console.log(para);
+// });
+// console.log(paras[2]);
+
+// const paras = document.querySelectorAll("p");
+// const errors = document.querySelectorAll(".error");
+
+// console.log(errors);
+
