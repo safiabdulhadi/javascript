4e0c8bb04f389b44e0226246ea0ae2edd0ad8110
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-04-07 23:48:39 +0200

    loop

--- youtube.js
+++ youtube.js
@@ -6,3 +6,69 @@
 // var str = "   Hello word" ;
 
 // console.log(str.trim());
+
+// function openWindow() {
+//   myWindow = window.open("https://www.google.com","","width=500px, height=500px , left =200px, top=200px");
+// }
+//  var myWindow;
+
+// function closeWindow() {
+//    myWindow.close();
+// // }
+
+// var string1 = "apple";
+// var string2 = "Banana";
+
+// string1 = string1.toLocaleLowerCase();
+// string2 = string2.toLocaleUpperCase();
+
+// document.write(string1 < string2);
+
+
+// var x = 10;
+
+// document.write(x++);
+// document.write(x);
+
+
+// var x = 2;
+// var y = x **3;
+
+// document.write(y)
+// if
+// if-else
+
+// let x = 15;
+// if(x > 10) {
+//     document.write('good');
+
+// }
+
+
+// var num = 7;
+// if(num % 2 == 0) {
+//     document.write("Even number");
+// } else {
+//     document.write("Odd number");
+// }
+
+// for(var num = 2;  num <= 20 ; num++) {
+//     if(num % 2 != 0) {
+//         document.write(num + "<br>");
+//     }
+// }
+
+
+// let listPrice = 120;
+// let salePrice = 100;
+// let discount = ((listPrice - salePrice) / listPrice) * 100;
+// discount = Math.round(discount);
+
+// document.write(discount);
+
+for(var x=1 ; x <= 5; x++) {
+    for(var y=1 ; y <= x; y++) {
+        document.write("*"); 
+      }
+      document.write("  <br/>");
+}
