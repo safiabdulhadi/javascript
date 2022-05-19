ccfcaa99e5e89c7bac5b809e3ba29ba048a0288a
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-05-19 21:43:06 +0200

    some exerices in js infor

--- Ulearna.js
+++ Ulearna.js
@@ -3,3 +3,28 @@
 // console.log(typeof Number(id));
 // let ids = +id;
 // console.log(typeof ids);
+
+
+// let is_out = true;
+// let str = String(is_out);
+// console.log(str);
+// console.log(typeof str);
+
+// alert("6" / "2");
+// let id = "34";
+// console.log(typeof id);
+// ids = Number(id);
+// console.log(typeof ids);
+
+// console.log(Boolean("Hello")); //true
+// console.log(Boolean("")); //false
+// console.log(Boolean(" "));//This is true because there is space 
+// result = prompt("hello",);
+// console.log(result);
+// let age = prompt('How old are you?', 100);
+
+// alert(`You are ${age} years old!`); // You are 100 years old!
+
+
+let age = prompt("How are you", "");
+console.log(`${age}`);
